46c3a1e64337803fe82d10d05c658d9ba11d598f
KJMAN <koji.smz@gmail.com>
2020-05-23 14:09:16 +0900

    add config

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+import flask_blog.views
+
 app = Flask(__name__)
 
-import flask_blog.views
+# flask_blogフォルダいかにあるconfig.pyの内容をconfigとして指定
+app.config.from_object('flask_blog.config')
