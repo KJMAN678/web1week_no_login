f575e26f65f794bb0bfb34a43d35d8a20e521a2d
KJMAN <koji.smz@gmail.com>
2020-05-23 14:14:46 +0900

    init narabikae

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-import flask_blog.views
-
 app = Flask(__name__)
 
 # flask_blogフォルダいかにあるconfig.pyの内容をconfigとして指定
 app.config.from_object('flask_blog.config')
+
+import flask_blog.views
