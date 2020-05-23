7012e7ef12e45cf7af17fb1f72c96e4ac3364558
KJMAN <koji.smz@gmail.com>
2020-05-23 14:22:48 +0900

    add index.html

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,7 +1,10 @@
+from flask import request, redirect, redirect, url_for, render_template, flash, session
+
 # __init__.py 内のappをインポート
 from flask_blog import app
 
 # URLにアクセスがあったときの処理
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    # templatesフォルダ以下にあるentries/index.html を返してレンダリングする
+    return render_template('entries/index.html')
